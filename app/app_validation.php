58b7ebb8f49d73a651c937eaefbcc147fec19396
ttsuruoka <tatsuya.tsuruoka@gmail.com>
2012-05-04 15:31:13 +0900

    removed unused code

--- app/app_validation.php
+++
@@ -1,6 +0,0 @@
-<?php
-function validate_between($check, $min, $max)
-{
-    $n = mb_strlen($check);
-    return $min <= $n && $n <= $max;
-}
--- app/helpers/html_helper.php
+++ app/helpers/html_helper.php
@@ -5,10 +5,3 @@ function eh($string)
     if (!isset($string)) return;
     echo htmlspecialchars($string, ENT_QUOTES);
 }
-
-function readable_comment($s)
-{
-    $s = htmlspecialchars($s, ENT_QUOTES);
-    $s = nl2br($s);
-    return $s;
-}
